1ed22e8f6c6ac850c3036ab55fbdb78cca7a2d8d
Harenth <harenthchandrasekar23@gmail.com>
2025-07-15 07:56:46 +0000

    changes made

--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ class Todo(BaseModel):
     description: Optional[str] = None
     completed: bool = False
 
-
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
