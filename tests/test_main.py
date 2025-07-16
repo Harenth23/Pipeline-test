222da4dae9df74839da594fe8e24dbdbec14be4b
Harenth <harenthchandrasekar23@gmail.com>
2025-07-16 05:55:27 +0000

    changes made

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,15 @@
 from app.main import app
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock
+import mongomock
+from unittest.mock import AsyncMock, patch
 
 client = TestClient(app)
 
-def test_create_and_get_todo(monkeypatch):
-    fake_insert = MagicMock(return_value={"inserted_id": "12345"})
-    monkeypatch.setattr("app.crud.db.todos.insert_one", fake_insert)
+@patch("app.crud.db.todos.insert_one", new_callable=AsyncMock)
+def test_create_and_get_todo(mock_insert):
+    mock_insert.return_value = type("obj", (object,), {"inserted_id": "12345"})
 
-    todo = {"title": "Test", "description": "Desc", "completed": False}
+    todo = {"title": "Test Todo", "description": "Write test", "completed": False}
     response = client.post("/todos/", json=todo)
 
     assert response.status_code == 200
